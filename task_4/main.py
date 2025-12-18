eff596b8f0419184a567b532c77cabe37bc99f89
krpcn1r <zaystep09@gmail.com>
2025-12-18 12:03:46 +0300

    Update main.py

--- task_4/main.py
+++ task_4/main.py
@@ -3,9 +3,11 @@ import requests
 import json
 
 URL = "https://www.scrapethissite.com/pages/simple/"
+# Загружаем содержимое страницы
 response = requests.get(URL)
 response.raise_for_status()
 
+# Создаем объект для поиска по HTML-тегам
 soup = bs(response.text, 'html.parser')
 countries = soup.find_all('div', class_='country')
 capitals = soup.find_all("div", class_ = "country-info")
@@ -13,22 +15,25 @@ capitals = soup.find_all("div", class_ = "country-info")
 data_countries = []
 data_capitals = []
 
+# Извлекаем названия стран и столиц в отдельные списки
 for i in countries:
     data_countries.append(i.find("h3", class_= "country-name").text.strip())
 
 for i in capitals:
     data_capitals.append(i.find("span", class_ = "country-capital").text.strip())
 
+# Объединяем два списка в один словарь {Страна: Столица}
 data_dict = dict(zip(data_countries, data_capitals))
 
 for i in range(len(data_countries)):
     print(f"{i + 1}. Country: {list(data_dict.keys())[i]}; Capital: {list(data_dict.values())[i]};")
 
-
+# Сохраняем собранные данные в файл JSON для долгосрочного хранения
 with open("C:/Users/ZaytsSte_89/Documents/Visual Studio 2022/Python/data.json", "w", encoding = "utf-8") as file:
     json.dump(data_dict, file, ensure_ascii=False, indent=4)
     print("Данные записаны")
 
+# Читаем данные обратно из файла для последующей обработки
 with open("C:/Users/ZaytsSte_89/Documents/Visual Studio 2022/Python/data.json", "r", encoding = "utf-8") as file:
     load_data = json.load(file)
     info = json.dumps(load_data, ensure_ascii=False)
@@ -38,6 +43,7 @@ with open("C:/Users/ZaytsSte_89/Documents/Visual Studio 2022/Python/data.json", 
 
     rows = ""
 
+    # Динамически формируем строки HTML-таблицы из данных словаря
     for a,b in zip(info_keys, info_values):
         row = f"""
         <tr>
@@ -51,6 +57,7 @@ with open("C:/Users/ZaytsSte_89/Documents/Visual Studio 2022/Python/data.json", 
         """
         rows += row
     
+    # Собираем финальный HTML-документ, вставляя в него готовую строку с рядами таблицы
     html = f"""
             <html>
                 <head>
@@ -79,5 +86,6 @@ with open("C:/Users/ZaytsSte_89/Documents/Visual Studio 2022/Python/data.json", 
             </html>
             """
 
+# Записываем итоговый HTML-код в файл для просмотра в браузере
 with open("C:/Users/ZaytsSte_89/Documents/Visual Studio 2022/Python/output.html", "w", encoding="utf-8") as file:
     file.write(html)
